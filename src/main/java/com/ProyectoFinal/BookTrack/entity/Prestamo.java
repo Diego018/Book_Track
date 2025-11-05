e70e76c1ba6b535ccc2fa7e179d17fe64f42e5df
Diego018 <diegoalejovv0116@gmail.com>
2025-11-05 16:47:40 -0500

    Creacion de las entidades + la conexion con la base de datos MYSQL - v1

---
+++ src/main/java/com/ProyectoFinal/BookTrack/entity/Prestamo.java
@@ -0,0 +1,32 @@
+package com.ProyectoFinal.BookTrack.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Prestamo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_prestamo;
+
+    private Date fechaPrestamo;
+    private Date fechaDevolucion;
+    private Boolean devuelto;
+
+    @ManyToOne(targetEntity = Usuario.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne(targetEntity = Libro.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_libro")
+    private Libro libro;
+
+}
